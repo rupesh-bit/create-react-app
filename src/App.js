7946bbe06a42441d58b67e6cd4ab2dc8f1dac073
rupesh <you@example.com>
2024-05-28 10:59:58 +0530

    jj

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
+    <div className="bg-blue-600">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="h-20" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
